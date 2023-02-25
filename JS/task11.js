b2c4b4d1daee4a11bf299695f83a15cd3f364323
OksanaKuzich <komoks255@gmail.com>
2023-02-25 19:41:47 +0100

    added lodash

--- JS/task11.js
+++ JS/task11.js
@@ -1,14 +1,37 @@
 // Дано два рядки. Чи можна перший рядок отримати з другим циклічним
 // зрушенням?
 
+// NATIVE
+
+// function reverse(str1, str2) {
+//   const strArr = str1.split("");
+
+//   const deleteEl = strArr.splice(0, 2);
+
+//   strArr.push(...deleteEl);
+
+//   const str = strArr.join("");
+
+//   if (str === str2) {
+//     return "TRUE";
+//   }
+//   return "FALSE";
+// }
+
+// console.log(reverse("kate", "teka"));
+
+// LODASH
+
+let _ = require("lodash");
+
 function reverse(str1, str2) {
-  const strArr = str1.split("");
+  const strArr = _.split(str1, "", str1.length);
 
-  const deleteEl = strArr.splice(0, 2);
+  const deleteEl = _.slice(strArr, [(start = 0)], [(end = 2)]);
+  const strArray = _.drop(strArr, 2);
+  const newArray = _.concat(strArray, deleteEl);
 
-  strArr.push(...deleteEl);
-
-  const str = strArr.join("");
+  const str = _.join(newArray, "");
 
   if (str === str2) {
     return "TRUE";
